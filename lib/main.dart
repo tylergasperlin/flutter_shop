365454759f3917dd22fae89776ba950475d16945
Tyler Gasperlin <gasperlint@gmail.com>
2021-10-25 19:02:45 -0700

    Add the product screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/screens/products_overview_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: ProductsOverViewScreen(),
     );
   }
 }
