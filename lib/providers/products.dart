5091d99d91f1387b157c86fd127c61fc5c42d9c1
Tyler Gasperlin <gasperlint@gmail.com>
2021-10-31 18:50:18 -0700

    Stateless widget to manage favorites

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -38,14 +38,30 @@ class Products with ChangeNotifier {
     ),
   ];
 
+  var _showFavoritesOnly = false;
+
   // We use a getter so we return a copy of this instead of a direct reference/ pointer
   // If we had a direct pointer we might be able to edit
   List<Product> get items {
+    if (_showFavoritesOnly) {
+      return _items.where((prodItem) => prodItem.isFavorite).toList();
+    }
+
     return [..._items];
   }
 
   Product findById(String id) {
     return _items.firstWhere((prod) => prod.id == id);
+  }
+
+  void showFavoritsOnly() {
+    _showFavoritesOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoritesOnly = false;
+    notifyListeners();
   }
 
   void addProduct() {
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/products.dart';
+import 'package:provider/provider.dart';
 import '../widgets/products_grid.dart';
+
+enum FilterOptions { Favorites, All }
 
 class ProductsOverViewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productsContainer = Provider.of<Products>(context, listen: false);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: <Widget>[
+          PopupMenuButton(
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                  child: Text('Only Favorites'),
+                  value: FilterOptions.Favorites),
+              PopupMenuItem(child: Text('Show all'), value: FilterOptions.All),
+            ],
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.Favorites) {
+                productsContainer.showFavoritsOnly();
+              } else {
+                productsContainer.showAll();
+              }
+            },
+          )
+        ],
       ),
       body: ProductsGrid(),
     );
