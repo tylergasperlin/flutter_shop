115461039abcbdb9543451ac7e347d60af190c51
Tyler Gasperlin <gasperlint@gmail.com>
2021-11-15 19:58:58 -0800

    Delete item

--- lib/widgets/user_product.item.dart
+++ lib/widgets/user_product.item.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/products.dart';
 import 'package:flutter_complete_guide/screens/edit_product_screen.dart';
+import 'package:provider/provider.dart';
 
 class UserProductItem extends StatelessWidget {
   final String id;
-
   final String title;
   final String imageUrl;
 
@@ -27,7 +28,9 @@ class UserProductItem extends StatelessWidget {
               color: Theme.of(context).primaryColor,
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              },
               icon: Icon(Icons.delete),
               color: Theme.of(context).errorColor,
             )
