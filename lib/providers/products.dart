36f387f073bce195920ba07dae595b4374926032
Tyler Gasperlin <gasperlint@gmail.com>
2021-11-23 10:17:08 -0800

    Update products

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -31,7 +31,7 @@ class Products with ChangeNotifier {
       final List<Product> loadedProducts = [];
       extractedData.forEach((prodId, prodData) {
         loadedProducts.add(Product(
-            id: prodData['id'],
+            id: prodId,
             title: prodData['title'],
             description: prodData['description'],
             price: prodData['price'],
@@ -73,9 +73,18 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(String id, Product newProduct) {
+  void updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
+      final url = Uri.parse(
+          'https://flutter-shop-6c8e1-default-rtdb.firebaseio.com/products/$id.json');
+      await http.patch(url,
+          body: json.encode({
+            'title': newProduct.title,
+            'description': newProduct.description,
+            'imageUrl': newProduct.imageUrl,
+            'price': newProduct.price
+          }));
       _items[prodIndex] = newProduct;
       notifyListeners();
     } else {
